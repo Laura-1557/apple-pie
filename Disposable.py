2fdfb75ffafd57988a0aa2082074d26248e54e01
Laura-1557 <94494994+Laura-1557@users.noreply.github.com>
2022-01-03 04:25:25 +0000

    Add files via upload
    simplified existing code and added some more options

--- Disposable.py
+++ Disposable.py
@@ -11,7 +11,7 @@ ingredients = [
 
 def main():
 
-    def add_to_shop():
+    def ask_to_shop():
         need_ingredient = input("Do you want to add this item to your shopping list for next time anyway? ")
         if need_ingredient in affirmative_answer:
             with open('Shopping_List.txt', 'r') as todo_file:
@@ -20,6 +20,15 @@ def main():
             with open('Shopping_List.txt', "w+") as todo_file:
                 todo_file.write(todo)
                 print("Ok, added to Shopping List")
+        else:
+            print("Ok.", end=" ")
+
+    def add_to_stock_list():
+        with open('Ingredients_in_stock.txt', 'r') as todo_file:
+            todo = todo_file.read()
+        todo = todo + ingredient_name + "\n"
+        with open('Ingredients_in_stock.txt', "w+") as todo_file:
+            todo_file.write(todo)
 
     ingredient_name = input("Which ingredients do you have? ")
 
@@ -27,27 +36,22 @@ def main():
         have_ingredient = input("Maybe we could use that. Do you want to add this to the recipe? Yes/No ")
         if have_ingredient in affirmative_answer:
             ingredients.append(ingredient_name)
-            with open('Ingredients_in_stock.txt', 'r') as todo_file:
-                todo = todo_file.read()
-            todo = todo + ingredient_name + "\n"
-            with open('Ingredients_in_stock.txt', "w+") as todo_file:
-                todo_file.write(todo)
+            add_to_stock_list()
             see_ingredient_list = input("Do you want to see your list? ")
             if see_ingredient_list in affirmative_answer:
-                    print(ingredients)
+                print(ingredients)
+            else:
+                print("Ok.", end=" ")
             main()
 
         else:
-            add_to_shop()
+            print("Ok.", end=" ")
+            ask_to_shop()
             main()
     else:
-        print(f"Great, {ingredient_name}! Let's add that to the list of things we have.")
-        with open('Ingredients_in_stock.txt', 'r') as todo_file:
-            todo = todo_file.read()
-        todo = todo + ingredient_name + "\n"
-        with open('Ingredients_in_stock.txt', "w+") as todo_file:
-            todo_file.write(todo)
-        add_to_shop()
+        print(f"Great, {ingredient_name}! Let's add that to the list of recipe ingredients we have in stock.")
+        add_to_stock_list()
+        ask_to_shop()
         main()
 
 
